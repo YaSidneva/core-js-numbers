6dd0d4f161bcf624e2f7a6173220ec2d9eddf41d
Yana Sidneva <yan-zublenk2008@narod.ru>
2023-11-20 10:24:32 +0100

    feat: add isPrime task solution

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -207,7 +207,12 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  return n > 1;
+  if (n === 2 || n === 3) return true;
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+  return true;
 }
 
 /**
